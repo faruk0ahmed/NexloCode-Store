f7fdac8e5be0070f818bb35b685b4666161bc0a9
faruk0ahmed <farukahmed2003online@gmail.com>
2025-12-05 11:00:37 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,16 @@
+var allApps = [
+    {
+        "id": 1764874062659,
+        "category": "app",
+        "name": "ITG: Invisible Text Generator",
+        "dev": "NexloCode",
+        "icon": "https://i.ibb.co.com/MDdKgf8L/logo.png",
+        "rating": "4.5",
+        "link": "https://www.mediafire.com/file/exhf1f446xjw1p4/ITG_Magic_Text.apk/file",
+        "screenshots": [
+            "https://i.ibb.co.com/rRNjdxJm/Screenshot-2025-12-05-00-33-24-791-com-nexlocode-itg.jpg",
+            "https://i.ibb.co.com/9mm36RRC/Screenshot-2025-12-05-00-33-35-626-com-nexlocode-itg.jpg"
+        ],
+        "desc": "This is a magical tool that hides secret messages inside normal text.\nOn Facebook or Messenger, everyone will see a regular story — but the person who knows the password will see the real hidden message.\n\nFeatures:\n\n1. Hidden Chat: Conceal secret text behind ordinary writing.\n2. Password Lock: The message stays locked and can only be revealed with the password.\n\nIn short: \"Normal on the outside, secret on the inside!\n---------\nএটি এমন একটি জাদুকরী টুল যা সাধারণ লেখার ভেতরে গোপন মেসেজ লুকিয়ে ফেলে। ফেসবুকে বা মেসেঞ্জারে সবাই দেখবে সাধারণ গল্প, কিন্তু পাসওয়ার্ড জানা ব্যক্তি দেখবে আসল গোপন কথা।\nফিচার:\n\n১. লুকানো চ্যাট: লেখার আড়ালে লেখা লুকানো যায়।\n২. পাসওয়ার্ড লক: মেসেজটি পাসওয়ার্ড দিয়ে লক করা থাকে।\n\nএক কথায়: \"সামনে সাধারণ গল্প, ভেতরে গোপন কথা!\""
+    }
+];
